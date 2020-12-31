7d270fbd08ac6910af1fb67572ea3ee755d11868
yU5295 <danielren0401@gmail.com>
2020-12-31 06:45:07 -0800

    fixing breakpoint

--- components/Article.vue
+++ components/Article.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div
-  .bg-white.rounded-xl.m-8.px-8.py-16.mt-8.grid.gap-10(class='lg:grid-cols-2')
-    section.transcript-container.overflow-y-auto.px-8
+  .bg-white.rounded-xl.m-8.px-8.py-16.mt-8.gap-10(class='md:grid lg:grid-cols-2')
+    section.transcript-container.overflow-y-auto.px-8.mb-8(class="md:mb-0")
       .mb-8.cursor-pointer.transcript(
         v-for='(t, index) in blok.transcript'
         :key='index'
