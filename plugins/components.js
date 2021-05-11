67b96d8637eed7cf91d953c775cfd357e1daefb7
yU5295 <danielren0401@gmail.com>
2021-05-10 19:18:11 -0700

    Removing unused component

--- plugins/components.js
+++ plugins/components.js
@@ -4,7 +4,6 @@ import InfoCard from '~/components/InfoCard.vue'
 import ArticleList from '~/components/ArticleList.vue'
 import FeaturedCard from '~/components/FeaturedCard.vue'
 import FeaturedImage from '~/components/FeaturedImage.vue'
-import FeaturedEvents from '~/components/FeaturedEvents.vue'
 import ArticlesTeaser from '~/components/ArticlesTeaser.vue'
 
 Vue.component('Grid', Grid)
@@ -12,5 +11,4 @@ Vue.component('InfoCard', InfoCard)
 Vue.component('ArticleList', ArticleList)
 Vue.component('FeaturedCard', FeaturedCard)
 Vue.component('FeaturedImage', FeaturedImage)
-Vue.component('FeaturedEvents', FeaturedEvents)
 Vue.component('ArticlesTeaser', ArticlesTeaser)
