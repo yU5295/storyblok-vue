2067302e5b69a6404a01e5a98f6f13f4e552dd9f
yU5295 <danielren0401@gmail.com>
2021-05-15 14:25:08 -0700

    Fixing mobile container

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  watchman: false,
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     // this line is optional and the tilde shortcut
     // will not be used in this tutorial
     '^~/(.*)$': '<rootDir>/$1'
   },
+  modulePathIgnorePatterns: ['<rootDir>/test/'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!vuex-composition-helpers/dist)'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
 
   collectCoverageFrom: [
     'components/**/*.vue',
