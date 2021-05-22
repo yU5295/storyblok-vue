503841b2a5b1dde5e0400eace6d3b63d58f96a53
yU5295 <danielren0401@gmail.com>
2021-05-21 18:41:39 -0700

    Styling language switcher

--- components/LanguageSwitch.vue
+++ components/LanguageSwitch.vue
@@ -1,6 +1,23 @@
 <template lang="pug">
-.flex
-  ul
-    li(v-for='lang in $i18n.locales' :key='lang.code' :class="{ 'active': $i18n.locale === lang.code }")
-      nuxt-link(:to="switchLocalePath(lang.code)") {{ lang.name }}
+.px-4.max-w-sm(class="md:px-0")
+  select.mt-1.block.w-full.px-1.py-2.text-base.bg-transparent.text-white.border.rounded-md(
+    @change="navigate($event)"
+    class='focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+  )
+    option(v-for='lang in $i18n.locales' :key='lang.code' :value="lang.code") {{ capitalize(lang.code) }}
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { useContext } from '~/hooks/useContext'
+
+export default defineComponent({
+  setup() {
+    const { proxy } = useContext()
+    const capitalize = (str: string): string => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
+    const navigate = (e: Event) => proxy.$router.push({ path: proxy.switchLocalePath((e.target as HTMLInputElement).value) })
+
+    return {  navigate, capitalize }
+  }
+})
+</script>
